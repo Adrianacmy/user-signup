d9a450de234258d70807eb145253d690aa2e46ff
Adrianacmy <adrianamullaney@gmail.com>
2017-06-30 20:28:37 -0700

    add style

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, redirect, render_template
 import re
-# app = Flask(__name__)
-# app.config['DEBUG'] = True
 
 
 app = Flask(__name__)
@@ -10,7 +8,7 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template('base.html', title='Hello, Word')
+    return render_template('welcome.html', title='Hello, Word')
 
 @app.route('/signup')
 def display_signup():
@@ -29,15 +27,18 @@ def signup():
     error_confirm_password = ''
     error_email = ''
 
-    if not re.match('^[a-zA-Z]\w{2,19}$', username):
+    if not re.match('^[a-zA-Z]\w{3,19}$', username):
         error_username = 'Must start with a letter and contains 3 to 20 characters(letters,underscores, numbers)'
         username = ''
-    if not re.match('^[a-zA-Z]\w{2,19}$', password):
+
+    if not re.match('^\w{3,19}$', password):
         error_password = 'Must start with letter, contains 3 to 20 characters(letters, numbers, underscores)'
         password = ''
+
     if confirm_password != password:
-        error_confirm_password = 'Password do not match'
+        error_confirm_password = 'Password does not match'
         confirm_password = ''
+
     if email:
         if not re.match('^\w+([-+]\w+)*@\w+([-]\w+)*\.\w+([-]\w+)*${2, 19}', email):
             email = ''
